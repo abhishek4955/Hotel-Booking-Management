8e8876018c15c77982e14228598947f25ad1bc1a
abhishek4955 <rohitkhadse450@gmail.com>
2024-09-08 15:44:47 +0530

    Ill be adding README.md file

---
+++ controllers/auth.js
@@ -0,0 +1,41 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const createError = require('../utils/error.js')
+const jwt = require('jsonwebtoken')
+const dotenv = require("dotenv");
+
+dotenv.config()
+module.exports.register = async (req, res, next) => {
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt)
+        const newUser = new User({
+            ...req.body,
+            password: hash,
+        });
+        await newUser.save();
+        res.status(200).send("User Has Been Created")
+    } catch (err){
+        next(err)
+    }
+}
+
+module.exports.login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.body.username});
+        if(!user) return next(createError(404, "User Not Found"))
+        
+        const isPasswordCorrect = await  bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return next(createError(403, 'Wrong Password'))
+
+        // Sign JWT token
+        const token = jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, process.env.JWTToken);
+
+        const { password, isAdmin, ...otherDetails } = user._doc;
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        }).status(200).json({details: {...otherDetails}, isAdmin, access_token: token}) 
+    } catch (err){
+        next(err)
+    }
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user.js");
+const authRoutes = require("./routes/auth.js");
+const hotelsRoutes = require("./routes/hotels.js");
+const roomRoutes = require("./routes/rooms.js");
+
+const app = express();
+dotenv.config();
+
+// Database Connection
+const db_connect = async () => {
+    try {
+        await mongoose.connect(process.env.URI);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err);
+        process.exit(1); // Exit the application if unable to connect to the database
+    }
+};
+
+// Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/hotels", hotelsRoutes)
+app.use("/api/rooms", roomRoutes)
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    db_connect();
+    console.log("Server started on port", PORT);
+});
